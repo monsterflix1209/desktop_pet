8fc895a5b468e164aa355bea154974eca2f7d1e1
monsterflix1209 <s1140259@gm.cjjh.tc.edu.tw>
2026-08-27 15:02:44 +0800

    Launch Liquid Glass V4 by default

--- run_ultimate.py
+++ run_ultimate.py
@@ -1,9 +1,5 @@
 import sys
-from PySide6.QtWidgets import QApplication, QStyle
-import desktop_pet_v3 as v3
-
-# Compatibility injection: V3 uses QStyle for the system-tray icon.
-v3.QStyle = QStyle
+import desktop_pet_v4 as v4
 
 if __name__ == '__main__':
-    sys.exit(v3.main())
+    sys.exit(v4.main())
